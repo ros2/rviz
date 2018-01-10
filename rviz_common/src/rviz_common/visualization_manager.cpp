3822108e8a8e4222a4d844242d0a7d9ca5a32422
Andreas Holzner <external.Andreas.Holzner@bosch-si.com>
2018-01-10 16:54:40 +0100

    Add the main rviz node to the executor

--- rviz_common/src/rviz_common/visualization_manager.cpp
+++ rviz_common/src/rviz_common/visualization_manager.cpp
@@ -233,6 +233,7 @@ VisualizationManager::VisualizationManager(
   selection_manager_ = std::make_shared<SelectionManager>(this);
   view_picker_ = std::make_shared<ViewPicker>(this);
 
+  ros_node_abstraction_.lock()->add_to_executor(*executor_);
 // TODO(wjwwood): redo with executors?
 #if 0
   private_->threaded_queue_threads_.create_thread(std::bind(&VisualizationManager::
