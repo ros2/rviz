fb8b1705aa020fdefd53cc866065bb9370c4ee40
Andreas Greimel <external.andreas.greimel@bosch-si.com>
2018-04-27 13:28:35 +0200

    Add Line object test

---
+++ rviz_rendering/test/rviz_rendering/objects/line_test.cpp
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2018, Bosch Software Innovations GmbH.
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted (subject to the limitations in the disclaimer
+ * below) provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the copyright holder nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
+#include <gtest/gtest.h>  // NOLINT
+
+#include <memory>
+#include <vector>
+
+#include <Ogre.h>
+
+#include "rviz_rendering/objects/line.hpp"
+
+#include "test/rviz_rendering/ogre_testing_environment.hpp"
+
+class LineTestFixture : public ::testing::Test
+{
+protected:
+  static void SetUpTestCase()
+  {
+    testing_environment_ = std::make_shared<rviz_rendering::OgreTestingEnvironment>();
+    testing_environment_->setUpOgreTestEnvironment();
+  }
+
+  static std::shared_ptr<rviz_rendering::OgreTestingEnvironment> testing_environment_;
+};
+
+std::shared_ptr<rviz_rendering::OgreTestingEnvironment> LineTestFixture::testing_environment_ =
+  nullptr;
+
+TEST_F(LineTestFixture, setPoints_sets_the_line_position_and_size) {
+  auto scene_manager = Ogre::Root::getSingletonPtr()->createSceneManager();
+  auto root_node = scene_manager->getRootSceneNode();
+
+  auto line = new rviz_rendering::Line(scene_manager, root_node);
+  line->setPoints(Ogre::Vector3(-5, -5, -5), Ogre::Vector3(3, 3, 3));
+
+  auto line_node = dynamic_cast<Ogre::SceneNode *>(root_node->getChild(0));
+  auto aabb = line_node->getAttachedObject(0)->getBoundingBox();
+
+  ASSERT_EQ(aabb.getMinimum().x, -5);
+  ASSERT_EQ(aabb.getMinimum().y, -5);
+  ASSERT_EQ(aabb.getMinimum().z, -5);
+
+  ASSERT_EQ(aabb.getMaximum().x, 3);
+  ASSERT_EQ(aabb.getMaximum().y, 3);
+  ASSERT_EQ(aabb.getMaximum().z, 3);
+}
