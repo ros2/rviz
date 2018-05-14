3248ecc2c261f7b29b368a81d03ee445f6f37bb2
Martin Idel <external.Martin.Idel@bosch-si.com>
2018-05-14 14:25:48 +0200

    Make style changes and refactoring of msg_conversions

--- rviz_common/include/rviz_common/msg_conversions.hpp
+++ rviz_common/include/rviz_common/msg_conversions.hpp
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 2012, Willow Garage, Inc.
+ * Copyright (c) 2018, Bosch Software Innovations GmbH.
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -26,94 +27,57 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
-#ifndef MSG_CONVERSIONS_H
-#define MSG_CONVERSIONS_H
+#ifndef RVIZ_COMMON__MSG_CONVERSIONS_HPP_
+#define RVIZ_COMMON__MSG_CONVERSIONS_HPP_
 
-#include "OgreVector3.h"
-#include "OgreQuaternion.h"
+#include <OgreVector3.h>
+#include <OgreQuaternion.h>
 
-#include <geometry_msgs/Point.h>
-#include <geometry_msgs/Vector3.h>
-#include <geometry_msgs/Quaternion.h>
+#include "geometry_msgs/msg/point.hpp"
+#include "geometry_msgs/msg/vector3.hpp"
+#include "geometry_msgs/msg/quaternion.hpp"
 
-namespace rviz {
+namespace rviz_common
+{
 
 // This file contains some convenience functions for Ogre / geometry_msgs conversions.
 
-// pointMsgToOgre  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-static inline Ogre::Vector3 pointMsgToOgre(const geometry_msgs::Point& m)
+static inline Ogre::Vector3 pointMsgToOgre(const geometry_msgs::msg::Point & m)
 {
   return Ogre::Vector3(m.x, m.y, m.z);
 }
 
-static inline void pointMsgToOgre(const geometry_msgs::Point &m, Ogre::Vector3& o)
-{
-  o.x = m.x; o.y = m.y; o.z = m.z;
-}
-
-// vector3MsgToOgre  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-static inline Ogre::Vector3 vector3MsgToOgre(const geometry_msgs::Vector3& m)
+static inline Ogre::Vector3 vector3MsgToOgre(const geometry_msgs::msg::Vector3 & m)
 {
   return Ogre::Vector3(m.x, m.y, m.z);
 }
 
-
-static inline void vector3MsgToOgre(const geometry_msgs::Vector3 &m, Ogre::Vector3& o)
-{
-  o.x = m.x; o.y = m.y; o.z = m.z;
-}
-
-// quaternionMsgToOgre  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-static inline Ogre::Quaternion quaternionMsgToOgre(const geometry_msgs::Quaternion& m)
+static inline Ogre::Quaternion quaternionMsgToOgre(const geometry_msgs::msg::Quaternion & m)
 {
   return Ogre::Quaternion(m.w, m.x, m.y, m.z);
 }
 
-static inline void quaternionMsgToOgre(const geometry_msgs::Quaternion &m, Ogre::Quaternion& o)
+static inline geometry_msgs::msg::Point pointOgreToMsg(const Ogre::Vector3 & o)
 {
-  o.w = m.w; o.x = m.x; o.y = m.y; o.z = m.z;
-}
-
-// pointOgreToMsg  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-static inline void pointOgreToMsg(const Ogre::Vector3 &o, geometry_msgs::Point &m)
-{
+  geometry_msgs::msg::Point m;
   m.x = o.x; m.y = o.y; m.z = o.z;
-}
-
-static inline geometry_msgs::Point pointOgreToMsg(const Ogre::Vector3 &o)
-{
-  geometry_msgs::Point m;
-  pointOgreToMsg(o, m);
   return m;
 }
 
-// vectorOgreToMsg  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-static inline void vector3OgreToMsg(const Ogre::Vector3 &o, geometry_msgs::Vector3 &m)
+static inline geometry_msgs::msg::Vector3 vector3OgreToMsg(const Ogre::Vector3 & o)
 {
+  geometry_msgs::msg::Vector3 m;
   m.x = o.x; m.y = o.y; m.z = o.z;
-}
-
-static inline geometry_msgs::Vector3 vector3OgreToMsg(const Ogre::Vector3 &o)
-{
-  geometry_msgs::Vector3 m;
-  vector3OgreToMsg(o, m);
   return m;
 }
 
-// quaternionOgreToMsg  - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - //
-static inline void quaternionOgreToMsg(const Ogre::Quaternion &o, geometry_msgs::Quaternion &m)
+static inline geometry_msgs::msg::Quaternion quaternionOgreToMsg(const Ogre::Quaternion & o)
 {
+  geometry_msgs::msg::Quaternion m;
   m.w = o.w; m.x = o.x; m.y = o.y; m.z = o.z;
-}
-
-static inline geometry_msgs::Quaternion quaternionOgreToMsg(const Ogre::Quaternion &o)
-{
-  geometry_msgs::Quaternion m;
-  quaternionOgreToMsg(o, m);
   return m;
 }
 
+}  // namespace rviz_common
 
-} // namespace rviz
-
-#endif
+#endif  // RVIZ_COMMON__MSG_CONVERSIONS_HPP_
