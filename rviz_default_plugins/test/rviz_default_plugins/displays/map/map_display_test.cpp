ab9af53c74cd7878c57ae360bcb205e910e7ef20
Martin Idel <external.Martin.Idel@bosch-si.com>
2018-06-14 09:39:06 +0200

    Fix includes

--- rviz_default_plugins/test/rviz_default_plugins/displays/map/map_display_test.cpp
+++ rviz_default_plugins/test/rviz_default_plugins/displays/map/map_display_test.cpp
@@ -29,7 +29,6 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#include <gtest/gtest.h>
 #include <gmock/gmock.h>
 
 #include <cmath>
@@ -40,7 +39,8 @@
 #include <OgreManualObject.h>
 
 #include "rviz_default_plugins/displays/map/map_display.hpp"
-#include "../../scene_graph_introspection.hpp"
+#include "test/rviz_rendering/scene_graph_introspection.hpp"
+#include "../../scene_graph_introspection_helper.hpp"
 #include "../display_test_fixture.hpp"
 
 using namespace ::testing;  // NOLINT
@@ -100,7 +100,7 @@ public:
   void expectNoManualObjects()
   {
     map_display_->showMap();
-    auto manual_objects = rviz_default_plugins::findAllOgreObjectByType<Ogre::ManualObject>(
+    auto manual_objects = rviz_rendering::findAllOgreObjectByType<Ogre::ManualObject>(
       scene_manager_->getRootSceneNode(), "ManualObject");
 
     EXPECT_THAT(manual_objects, IsEmpty());
@@ -147,7 +147,7 @@ TEST_F(MapTestFixture, showMap_shows_manual_object_if_current_map_is_valid) {
 
   map_display_->processMessage(createMapMessage());
 
-  auto manual_objects = rviz_default_plugins::findAllOgreObjectByType<Ogre::ManualObject>(
+  auto manual_objects = rviz_rendering::findAllOgreObjectByType<Ogre::ManualObject>(
     scene_manager_->getRootSceneNode(), "ManualObject");
 
   ASSERT_THAT(manual_objects, SizeIs(1));
@@ -168,7 +168,7 @@ TEST_F(MapTestFixture, showMap_defaults_empty_frame_id_to_map) {
   message->header.frame_id = "";
   map_display_->processMessage(message);
 
-  auto manual_objects = rviz_default_plugins::findAllOgreObjectByType<Ogre::ManualObject>(
+  auto manual_objects = rviz_rendering::findAllOgreObjectByType<Ogre::ManualObject>(
     scene_manager_->getRootSceneNode(), "ManualObject");
 
   ASSERT_THAT(manual_objects, SizeIs(1));
@@ -181,7 +181,7 @@ TEST_F(MapTestFixture, showMap_defaults_empty_frame_id_to_map) {
 TEST_F(MapTestFixture, showMap_does_not_display_anything_if_transform_fails) {
   map_display_->processMessage(createMapMessage());
 
-  auto manual_objects = rviz_default_plugins::findAllOgreObjectByType<Ogre::ManualObject>(
+  auto manual_objects = rviz_rendering::findAllOgreObjectByType<Ogre::ManualObject>(
     scene_manager_->getRootSceneNode(), "ManualObject");
 
   ASSERT_THAT(manual_objects, SizeIs(1));
@@ -197,7 +197,7 @@ TEST_F(MapTestFixture, reset_makes_map_invisible) {
 
   map_display_->reset();
 
-  auto manual_objects = rviz_default_plugins::findAllOgreObjectByType<Ogre::ManualObject>(
+  auto manual_objects = rviz_rendering::findAllOgreObjectByType<Ogre::ManualObject>(
     scene_manager_->getRootSceneNode(), "ManualObject");
 
   ASSERT_THAT(manual_objects, SizeIs(1));
@@ -207,7 +207,7 @@ TEST_F(MapTestFixture, reset_makes_map_invisible) {
 TEST_F(MapTestFixture, createSwatches_creates_more_swatches_if_map_is_too_big) {
   map_display_->processMessage(createMapMessage(500, 500));
 
-  auto manual_objects = rviz_default_plugins::findAllOgreObjectByType<Ogre::ManualObject>(
+  auto manual_objects = rviz_rendering::findAllOgreObjectByType<Ogre::ManualObject>(
     scene_manager_->getRootSceneNode(), "ManualObject");
 
   EXPECT_THAT(manual_objects, SizeIs(4));
