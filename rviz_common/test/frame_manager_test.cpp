f7d0e9cb05e2e79b4d1d1ac6a569520365ab5f1e
Martin Idel <external.Martin.Idel@bosch-si.com>
2018-07-19 15:13:23 +0200

    Add tests for interface in frame_manager

--- rviz_common/test/frame_manager_test.cpp
+++ rviz_common/test/frame_manager_test.cpp
@@ -27,58 +27,42 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef RVIZ_COMMON__FRAME_TRANSFORMER_HPP_
-#define RVIZ_COMMON__FRAME_TRANSFORMER_HPP_
+#include <gmock/gmock.h>
 
 #include <memory>
-#include <string>
-#include <vector>
 
-#include "geometry_msgs/msg/pose_stamped.hpp"
-#include "geometry_msgs/msg/transform_stamped.hpp"
+#include "rclcpp/rclcpp.hpp"
 
-#include "rviz_common/ros_integration/ros_node_abstraction.hpp"
+#include "../src/rviz_common/frame_manager.hpp"
+#include "mock_frame_transformer.hpp"
 
-namespace rviz_common
-{
-struct InternalFrameTransformer
-{
-  virtual ~InternalFrameTransformer() = default;
-};
+using namespace ::testing;
 
-using InternalFrameTransformerPtr = std::shared_ptr<InternalFrameTransformer>;
 
-class FrameTransformer
-{
-public:
-  virtual void initialize(ros_integration::RosNodeAbstractionIface::WeakPtr rviz_ros_node) = 0;
+TEST(FrameManager, transform_uses_transform_of_transformer) {
+  auto clock = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
+  auto frame_transformer = std::make_shared<MockFrameTransformer>();
+  EXPECT_CALL(*frame_transformer, transform(_,_,_)).WillOnce(Return(true));
+  auto frame_manager = std::make_shared<rviz_common::FrameManager>(clock, frame_transformer);
 
-  virtual void clear() = 0;
+  geometry_msgs::msg::Pose pose;
+  Ogre::Vector3 position;
+  Ogre::Quaternion orientation;
+  frame_manager->transform(
+    "any_frame", rclcpp::Time(0, 0, clock->get_clock_type()), pose, position, orientation);
+}
 
-  virtual std::vector<std::string> getAllFrameNames() = 0;
+TEST(FrameManager, lastAvailableTransform_used_when_using_sync_approx) {
+  auto clock = std::make_shared<rclcpp::Clock>(RCL_ROS_TIME);
+  auto frame_transformer = std::make_shared<MockFrameTransformer>();
+  EXPECT_CALL(*frame_transformer, lastAvailableTransform(_,_,_)).WillOnce(Return(true));
+  EXPECT_CALL(*frame_transformer, transform(_,_,_)).WillOnce(Return(true));
+  auto frame_manager = std::make_shared<rviz_common::FrameManager>(clock, frame_transformer);
 
-  virtual bool transform(
-    // NOLINT (this is not std::transform)
-    const geometry_msgs::msg::PoseStamped & pose_in,
-    geometry_msgs::msg::PoseStamped & pose_out,
-    const std::string & frame) = 0;
-
-  virtual bool lastAvailableTransform(
-    const std::string & target_frame,
-    const std::string & source_frame,
-    geometry_msgs::msg::TransformStamped & transform) = 0;
-
-  virtual bool transformHasProblems(
-    const std::string & frame,
-    const std::string & fixed_frame,
-    const rclcpp::Time & time,
-    std::string & error) = 0;
-
-  virtual bool frameHasProblems(const std::string & frame, std::string & error) = 0;
-
-  /// Expose internal implementation
-  virtual InternalFrameTransformerPtr getInternals() = 0;
-};
-}  // namespace rviz_common
-
-#endif  // RVIZ_COMMON__FRAME_TRANSFORMER_HPP_
+  frame_manager->setSyncMode(rviz_common::FrameManager::SyncApprox);
+  geometry_msgs::msg::Pose pose;
+  Ogre::Vector3 position;
+  Ogre::Quaternion orientation;
+  frame_manager->transform(
+    "any_frame", rclcpp::Time(0, 0, clock->get_clock_type()), pose, position, orientation);
+}
