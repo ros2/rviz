329947a3fb7b488a80d836987f203d8e63622c91
Martin Idel <external.Martin.Idel@bosch-si.com>
2018-08-06 09:06:27 +0200

    Move shortcut_key initialization to constructor

--- rviz_default_plugins/src/rviz_default_plugins/tools/focus/focus_tool.cpp
+++ rviz_default_plugins/src/rviz_default_plugins/tools/focus/focus_tool.cpp
@@ -67,14 +67,14 @@ namespace tools
 
 FocusTool::FocusTool()
 : Tool()
-{}
+{
+  shortcut_key_ = 'c';
+}
 
 FocusTool::~FocusTool() = default;
 
 void FocusTool::onInitialize()
 {
-  shortcut_key_ = 'c';
-
   std_cursor_ = rviz_common::getDefaultCursor();
   hit_cursor_ = rviz_common::makeIconCursor("package://rviz_common/icons/crosshair.svg");
 }
