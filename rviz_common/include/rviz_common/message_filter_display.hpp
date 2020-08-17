2e396a8b5df66562ed62a55e4c24107c86a3f5ba
Audrow Nash <audrow.nash@gmail.com>
2020-08-17 11:10:43 -0700

    Give message filter queue size UI a min value
    Signed-off-by: Audrow Nash <audrow.nash@gmail.com>

--- rviz_common/include/rviz_common/message_filter_display.hpp
+++ rviz_common/include/rviz_common/message_filter_display.hpp
@@ -70,7 +70,8 @@ public:
     message_queue_property_ = new properties::IntProperty(
       "Filter size", 10,
       "Set the filter size of the Message Filter Display.",
-      topic_property_, SLOT(updateMessageQueueSize()), this);
+      topic_property_, SLOT(updateMessageQueueSize()), this,
+      1, INT_MAX);
   }
 
   /**
