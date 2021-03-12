52f43c240967e4626a187aea30395efb7ddc4ce2
Victor Lamoine <VictorLamoine@users.noreply.github.com>
2021-03-12 19:03:09 +0100

    Allow to zoom more with orbit controller (#654)

--- rviz_default_plugins/src/rviz_default_plugins/view_controllers/orbit/orbit_view_controller.cpp
+++ rviz_default_plugins/src/rviz_default_plugins/view_controllers/orbit/orbit_view_controller.cpp
@@ -72,7 +72,7 @@ OrbitViewController::OrbitViewController()
   distance_property_ = new FloatProperty(
     "Distance", DISTANCE_START,
     "Distance from the focal point.", this);
-  distance_property_->setMin(0.01f);
+  distance_property_->setMin(0.001f);
 
   focal_shape_size_property_ = new FloatProperty(
     "Focal Shape Size", FOCAL_SHAPE_SIZE_START,
