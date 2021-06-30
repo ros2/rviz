e5f82ed9d3188e0c0c5b96f0eb2295e7a2887a8f
ahcorde <ahcorde@gmail.com>
2021-06-30 17:07:57 +0200

    fixed windows test
    Signed-off-by: ahcorde <ahcorde@gmail.com>

--- rviz_rendering/include/rviz_rendering/render_system.hpp
+++ rviz_rendering/include/rviz_rendering/render_system.hpp
@@ -91,10 +91,12 @@ public:
   prepareOverlays(Ogre::SceneManager * scene_manager);
 
   /// return OpenGl Version as integer, e.g. 320 for OpenGl 3.20
+  RVIZ_RENDERING_PUBLIC
   int
   getGlVersion();
 
   /// return GLSL Version as integer, e.g. 150 for GLSL 1.50
+  RVIZ_RENDERING_PUBLIC
   int
   getGlslVersion();
 
