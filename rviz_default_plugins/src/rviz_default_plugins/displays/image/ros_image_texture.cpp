f2a85b9a7370b1b367130780f7056cd5644c00a7
Caleb Turcotte <cturcotte@blackberry.com>
2021-07-22 11:12:30 -0400

    adjustments for ABI

--- rviz_default_plugins/src/rviz_default_plugins/displays/image/ros_image_texture.cpp
+++ rviz_default_plugins/src/rviz_default_plugins/displays/image/ros_image_texture.cpp
@@ -48,6 +48,9 @@
 #include "rviz_common/logging.hpp"
 #include "rviz_common/uniform_string_stream.hpp"
 
+uint32_t stride_;
+std::shared_ptr<std::vector<uint8_t>> bufferptr_;
+
 namespace rviz_default_plugins
 {
 namespace displays
@@ -75,6 +78,7 @@ ROSImageTexture::ROSImageTexture()
 ROSImageTexture::~ROSImageTexture()
 {
   current_image_.reset();
+  bufferptr_.reset();
 }
 
 void ROSImageTexture::clear()
