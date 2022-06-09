3584a2981d9b49a53d03dc243ce7be8dc142b758
juchajam <79612680+msjunechang21@users.noreply.github.com>
2022-06-10 00:14:54 +0900

    Add RVIZ_COMMON_PUBLIC macro (#865)

--- rviz_common/include/rviz_common/visualization_frame.hpp
+++ rviz_common/include/rviz_common/visualization_frame.hpp
@@ -80,7 +80,7 @@ class WidgetGeometryChangeDetector;
  * At the top is a toolbar with Tools like "Move Camera", "Select", etc.
  * There is also a menu bar with file/open, etc.
  */
-class VisualizationFrame : public QMainWindow, public WindowManagerInterface
+class RVIZ_COMMON_PUBLIC VisualizationFrame : public QMainWindow, public WindowManagerInterface
 {
   Q_OBJECT
 
