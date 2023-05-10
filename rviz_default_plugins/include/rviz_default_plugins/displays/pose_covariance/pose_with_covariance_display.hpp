0f19e9f5cfba6a9665ba614264ea3987e5beca65
Hyunseok <hyunseok7.yang@lge.com>
2023-05-10 17:00:47 +0900

    Update pose_with_covariance_display.hpp

--- rviz_default_plugins/include/rviz_default_plugins/displays/pose_covariance/pose_with_covariance_display.hpp
+++ rviz_default_plugins/include/rviz_default_plugins/displays/pose_covariance/pose_with_covariance_display.hpp
@@ -89,6 +89,7 @@ public:
 protected:
   /** @brief Overridden from MessageFilterDisplay to get Arrow/Axes visibility correct. */
   void onEnable() override;
+  void processMessage(geometry_msgs::msg::PoseWithCovarianceStamped::ConstSharedPtr message) override;
 
 private Q_SLOTS:
   void updateShapeVisibility();
@@ -99,8 +100,6 @@ private Q_SLOTS:
   void updateCovariance();
 
 private:
-  void
-  processMessage(geometry_msgs::msg::PoseWithCovarianceStamped::ConstSharedPtr message) override;
   void setupSelectionHandler();
 
   std::shared_ptr<rviz_rendering::Arrow> arrow_;
