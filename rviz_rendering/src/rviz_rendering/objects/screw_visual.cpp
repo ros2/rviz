130ed3696a4571f23ba0d14dd9dd93f02c933308
Alejandro Hernández Cordero <ahcorde@gmail.com>
2023-06-20 00:06:21 +0200

    Try to fix windows buil
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- rviz_rendering/src/rviz_rendering/objects/screw_visual.cpp
+++ rviz_rendering/src/rviz_rendering/objects/screw_visual.cpp
@@ -26,10 +26,11 @@
  * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
  * POSSIBILITY OF SUCH DAMAGE.
  */
+
+#define _USE_MATH_DEFINES
 #include <OgreSceneNode.h>
 #include <OgreSceneManager.h>
 
-#define _USE_MATH_DEFINES
 #include <cmath>
 
 #include "rviz_rendering/objects/arrow.hpp"
