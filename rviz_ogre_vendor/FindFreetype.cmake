ros2/rviz

commit 4e06de1cf277b5cf5eb777c084e23a92545a0889

Switch to ament_cmake_vendor_package (#995)
Signed-off-by: Scott K Logan <logans@cottsay.net>

rviz_ogre_vendor/CMakeLists.txt
@@ -1,249 +1,111 @@
 cmake_minimum_required(VERSION 3.10)
-
 project(rviz_ogre_vendor)
 
 find_package(ament_cmake REQUIRED)
-
-set(PACKAGE_VERSION "1.0.0")
-
-# Avoid DOWNLOAD_EXTRACT_TIMESTAMP warning for CMake >= 3.24
-if(POLICY CMP0135)
-  cmake_policy(SET CMP0135 NEW)
-endif()
+find_package(ament_cmake_vendor_package REQUIRED)
 
 if(WIN32)
-  set(BUILDING_FREETYPE_LOCALLY ON)
-  set(BUILDING_ZLIB_LOCALLY ON)
-else()
-  set(BUILDING_FREETYPE_LOCALLY OFF)
-  set(BUILDING_ZLIB_LOCALLY OFF)
-endif()
-
-if(WIN32 AND NOT ${CMAKE_VERBOSE_MAKEFILE})
-  set(should_log ON)  # prevent warnings in Windows CI
-else()
-  set(should_log OFF)
-endif()
-
-macro(build_freetype)
-  set(extra_cmake_args)
-  set(FREETYPE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-
-  if(WIN32)
-    list(APPEND FREETYPE_CXX_FLAGS "/w")
-    list(APPEND extra_cmake_args "-DCMAKE_C_FLAGS=/w")
+  set(FREETYPE_C_FLAGS "${CMAKE_C_FLAGS}")
+  if(MSVC)
+    set(FREETYPE_C_FLAGS "/wd4244 ${FREETYPE_C_FLAGS}")
+    set(FREETYPE_C_FLAGS "/wd4267 ${FREETYPE_C_FLAGS}")
+    set(FREETYPE_C_FLAGS "/wd4312 ${FREETYPE_C_FLAGS}")
+    set(FREETYPE_C_FLAGS "/wd4996 ${FREETYPE_C_FLAGS}")
   endif()
 
-  if(DEFINED CMAKE_TOOLCHAIN_FILE)
-    list(APPEND extra_cmake_args "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}")
-  else()
-    list(APPEND extra_cmake_args "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
-    list(APPEND extra_cmake_args "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}")
-  endif()
-  list(APPEND extra_cmake_args "-DCMAKE_CXX_FLAGS=${FREETYPE_CXX_FLAGS}")
-
-  include(ExternalProject)
-  ExternalProject_Add(freetype-2.11.1
-    URL https://git.savannah.gnu.org/cgit/freetype/freetype2.git/snapshot/freetype2-VER-2-11-1.tar.gz
-    URL_MD5 d2892e2c30b252039f54775ae8dc97c4
-    TIMEOUT 600
-    LOG_CONFIGURE ${should_log}
-    LOG_BUILD ${should_log}
+  ament_vendor(freetype_vendor
+    VCS_URL https://git.savannah.gnu.org/git/freetype/freetype2.git
+    VCS_VERSION VER-2-11-1
     CMAKE_ARGS
-      -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/freetype_install
-      ${extra_cmake_args}
-      -Wno-dev
-      -DWITH_ZLIB=OFF
-      -DWITH_BZip2=OFF
-      -DWITH_PNG=OFF
-      -DWITH_HarfBuzz=OFF
+      -DFT_DISABLE_ZLIB:BOOL=ON
+      -DFT_DISABLE_BZIP2:BOOL=ON
+      -DFT_DISABLE_PNG:BOOL=ON
+      -DFT_DISABLE_HARFBUZZ:BOOL=ON
+      "-DCMAKE_C_FLAGS=${FREETYPE_C_FLAGS}"
   )
 
-  install(
-    DIRECTORY
-      ${CMAKE_CURRENT_BINARY_DIR}/freetype_install/
-    DESTINATION
-      ${CMAKE_INSTALL_PREFIX}/opt/rviz_ogre_vendor
-    USE_SOURCE_PERMISSIONS
-  )
-endmacro()
-
-macro(build_zlib)
-  set(extra_cmake_args)
-  set(ZLIB_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-
-  if(WIN32)
-    list(APPEND ZLIB_CXX_FLAGS "/w")
-    list(APPEND extra_cmake_args "-DCMAKE_C_FLAGS=/w")
+  set(ZLIB_C_FLAGS "${ZLIB_C_FLAGS}")
+  if(MSVC)
+    set(ZLIB_C_FLAGS "/wd4267 ${ZLIB_C_FLAGS}")
   endif()
 
-  if(DEFINED CMAKE_TOOLCHAIN_FILE)
-    list(APPEND extra_cmake_args "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}")
-  else()
-    list(APPEND extra_cmake_args "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
-    list(APPEND extra_cmake_args "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}")
-  endif()
-  list(APPEND extra_cmake_args "-DCMAKE_CXX_FLAGS=${ZLIB_CXX_FLAGS}")
-
-  include(ExternalProject)
-  ExternalProject_Add(zlib-1.2.13
-    URL https://www.zlib.net/fossils/zlib-1.2.13.tar.gz
-    URL_MD5 9b8aa094c4e5765dabf4da391f00d15c
-    TIMEOUT 600
-    LOG_CONFIGURE ${should_log}
-    LOG_BUILD ${should_log}
+  ament_vendor(zlib_vendor
+    VCS_URL https://github.com/madler/zlib.git
+    VCS_VERSION v1.2.13
     CMAKE_ARGS
-      -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/zlib-install
-      ${extra_cmake_args}
-      -Wno-dev
+      # zlib doesn't use CMAKE_INSTALL_PREFIX correctly, so we need to override
+      -DINSTALL_BIN_DIR=<INSTALL_DIR>/bin
+      -DINSTALL_LIB_DIR=<INSTALL_DIR>/lib
+      -DINSTALL_INC_DIR=<INSTALL_DIR>/include
+      -DINSTALL_MAN_DIR=<INSTALL_DIR>/share/man
+      -DINSTALL_PKGCONFIG_DIR=<INSTALL_DIR>/share/pkgconfig
+      "-DCMAKE_C_FLAGS=${ZLIB_C_FLAGS}"
   )
-
-  install(
-    DIRECTORY
-      ${CMAKE_CURRENT_BINARY_DIR}/zlib-install/
-    DESTINATION
-      ${CMAKE_INSTALL_PREFIX}/opt/rviz_ogre_vendor
-    USE_SOURCE_PERMISSIONS
-  )
-endmacro()
-
-macro(build_ogre)
-  set(extra_cmake_args)
-  set(OGRE_C_FLAGS ${CMAKE_C_FLAGS})
-  set(OGRE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
-  # standard library is important for linking, but the other cxx flags are not
-  if(CMAKE_CXX_FLAGS MATCHES "-stdlib=libc\\+\\+")
-    set(OGRE_CXX_FLAGS "${OGRE_CXX_FLAGS} -stdlib=libc++")
-  endif()
-
-  if(DEFINED CMAKE_BUILD_TYPE)
-    list(APPEND extra_cmake_args -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE})
-  endif()
-
-  if(WIN32)
-    set(OGRE_C_FLAGS "${OGRE_C_FLAGS} /w /EHsc")
-    set(OGRE_CXX_FLAGS "${OGRE_CXX_FLAGS} /w /EHsc")
-  elseif(APPLE)
-    set(OGRE_CXX_FLAGS "${OGRE_CXX_FLAGS} -std=c++14 -stdlib=libc++ -w")
-    if (${CMAKE_SYSTEM_PROCESSOR} STREQUAL "arm64")
-      list(APPEND extra_cmake_args "-DCMAKE_OSX_ARCHITECTURES='arm64'")
-    else()
-      list(APPEND extra_cmake_args "-DCMAKE_OSX_ARCHITECTURES='x86_64'")
-    endif()
-  else()  # Linux
-    set(OGRE_C_FLAGS "${OGRE_C_FLAGS} -w")
-    # include Clang -Wno-everything to disable warnings in that build. GCC doesn't mind it
-    set(OGRE_CXX_FLAGS "${OGRE_CXX_FLAGS} -std=c++14 -w -Wno-everything")
-  endif()
-  list(APPEND extra_cmake_args "-DOGRE_BUILD_RENDERSYSTEM_GL:BOOL=TRUE")
-  list(APPEND extra_cmake_args "-DOGRE_BUILD_RENDERSYSTEM_D3D11:BOOL=OFF")
-  list(APPEND extra_cmake_args "-DOGRE_BUILD_RENDERSYSTEM_D3D9:BOOL=OFF")
-  list(APPEND extra_cmake_args "-DCMAKE_C_FLAGS=${OGRE_C_FLAGS}")
-  list(APPEND extra_cmake_args "-DCMAKE_CXX_FLAGS=${OGRE_CXX_FLAGS}")
-
-  # Uncomment this line to enable the GL3PLUS render engine
-  # list(APPEND extra_cmake_args "-DOGRE_BUILD_RENDERSYSTEM_GL3PLUS:BOOL=TRUE")
-
-  # These next lines can be used to disable some of the components or plugins for OGRE
-  list(APPEND extra_cmake_args "-DOGRE_CONFIG_ENABLE_ZIP:BOOL=ON")
-
-  if(BUILDING_FREETYPE_LOCALLY)
-    list(APPEND extra_cmake_args
-      "-DFREETYPE_HOME=${CMAKE_CURRENT_BINARY_DIR}/freetype_install")
-  endif()
-  if(BUILDING_ZLIB_LOCALLY)
-    list(APPEND extra_cmake_args
-      "-DZLIB_ROOT=${CMAKE_CURRENT_BINARY_DIR}/zlib-install")
-  endif()
-
-  if(DEFINED CMAKE_TOOLCHAIN_FILE)
-    list(APPEND extra_cmake_args "-DCMAKE_TOOLCHAIN_FILE=${CMAKE_TOOLCHAIN_FILE}")
-  else()
-    list(APPEND extra_cmake_args "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
-    list(APPEND extra_cmake_args "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}")
-  endif()
-
-  find_package(Patch REQUIRED)
-  include(ExternalProject)
-  ExternalProject_Add(ogre-v1.12.10
-    URL https://github.com/OGRECave/ogre/archive/v1.12.10.zip
-    URL_MD5 c1b870955efddf539385094e9034e7f7
-    TIMEOUT 1200
-    LOG_CONFIGURE ${should_log}
-    LOG_BUILD ${should_log}
-    CMAKE_ARGS
-      -DOGRE_STATIC:BOOL=OFF
-      -DOGRE_DEPENDENCIES_DIR=${CMAKE_CURRENT_BINARY_DIR}/ogredeps
-      -DOGRE_INSTALL_PDB:BOOL=OFF
-      -DOGRE_BUILD_DEPENDENCIES:BOOL=OFF
-      -DOGRE_BUILD_TESTS:BOOL=OFF
-      -DOGRE_BUILD_SAMPLES:BOOL=FALSE
-      -DOGRE_INSTALL_SAMPLES:BOOL=FALSE
-      -DOGRE_INSTALL_SAMPLES_SOURCE:BOOL=FALSE
-      -DOGRE_CONFIG_THREADS:STRING=0
-      -DOGRE_RESOURCEMANAGER_STRICT:STRING=2
-      -DCMAKE_SKIP_INSTALL_RPATH:BOOL=ON
-      -DCMAKE_STAGING_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/ogre_install
-      -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}/opt/rviz_ogre_vendor
-      -DOGRE_BUILD_LIBS_AS_FRAMEWORKS:BOOL=OFF
-      -DOGRE_BUILD_COMPONENT_PYTHON:BOOL=FALSE
-      -DOGRE_BUILD_COMPONENT_JAVA:BOOL=FALSE
-      -DOGRE_BUILD_COMPONENT_CSHARP:BOOL=FALSE
-      -DOGRE_BUILD_COMPONENT_BITES:BOOL=FALSE
-      -DOGRE_BUILD_PLUGIN_DOT_SCENE:BOOL=FALSE
-      -DOGRE_BUILD_TOOLS:BOOL=FALSE
-      ${extra_cmake_args}
-      -Wno-dev
-    PATCH_COMMAND
-      ${Patch_EXECUTABLE} -p1 -N -l < ${CMAKE_CURRENT_SOURCE_DIR}/pragma-patch.diff &&
-      ${Patch_EXECUTABLE} -p1 -N < ${CMAKE_CURRENT_SOURCE_DIR}/relocatable.patch &&
-      ${Patch_EXECUTABLE} -p1 -N < ${CMAKE_CURRENT_SOURCE_DIR}/featuresummary-rename.patch &&
-      ${CMAKE_COMMAND} -E rename CMake/FeatureSummary.cmake CMake/OgreFeatureSummary.cmake &&
-      ${Patch_EXECUTABLE} -p1 -N < ${CMAKE_CURRENT_SOURCE_DIR}/C4267.patch
-    COMMAND
-      ${CMAKE_COMMAND} -E copy ${CMAKE_CURRENT_SOURCE_DIR}/FindFreetype.cmake ${CMAKE_CURRENT_BINARY_DIR}/ogre-v1.12.10-prefix/src/ogre-v1.12.10/CMake/Packages/FindFreetype.cmake
-  )
-
-  if(BUILDING_FREETYPE_LOCALLY)
-    add_dependencies(ogre-v1.12.10 freetype-2.11.1)
-  endif()
-  if(BUILDING_ZLIB_LOCALLY)
-    add_dependencies(ogre-v1.12.10 zlib-1.2.13)
-  endif()
-
-  # The external project will install to the build folder, but we'll install that on make install.
-  install(
-    DIRECTORY
-      ${CMAKE_CURRENT_BINARY_DIR}/ogre_install/
-    DESTINATION
-      opt/rviz_ogre_vendor
-    USE_SOURCE_PERMISSIONS
-  )
-endmacro()
-
-if(BUILDING_FREETYPE_LOCALLY)
-  build_freetype()
-endif()
-if(BUILDING_ZLIB_LOCALLY)
-  build_zlib()
 endif()
 
-build_ogre()
-
-if(WIN32)
-  ament_environment_hooks(env_hook/rviz_ogre_vendor_library_path.bat)
-  set(ENV_VAR_NAME "PATH")
-  set(ENV_VAR_VALUE "opt\\rviz_ogre_vendor\\bin")
+set(OGRE_CXX_FLAGS ${CMAKE_CXX_FLAGS})
+if(MSVC)
+  set(OGRE_CXX_FLAGS "/wd4101 ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "/wd4244 ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "/wd4267 ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "/wd4334 ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "/wd4477 ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "/wd4996 ${OGRE_CXX_FLAGS}")
 else()
-  ament_environment_hooks(env_hook/rviz_ogre_vendor_library_path.sh)
-  if(APPLE)
-    set(ENV_VAR_NAME "DYLD_LIBRARY_PATH")
-  else()
-    set(ENV_VAR_NAME "LD_LIBRARY_PATH")
-  endif()
-  set(ENV_VAR_VALUE "opt/rviz_ogre_vendor/lib")
+  set(OGRE_CXX_FLAGS "-Wno-array-compare ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "-Wno-dangling-pointer ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "-Wno-deprecated-declarations ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "-Wno-format-overflow ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "-Wno-maybe-uninitialized ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "-Wno-mismatched-new-delete ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "-Wno-range-loop-construct ${OGRE_CXX_FLAGS}")
+  set(OGRE_CXX_FLAGS "-Wno-undef ${OGRE_CXX_FLAGS}")
 endif()
-ament_environment_hooks(env_hook/rviz_ogre_vendor_library_path.dsv.in)
+
+set(OGRE_CMAKE_ARGS)
+if(NOT WIN32)
+  list(APPEND OGRE_CMAKE_ARGS -DCMAKE_SKIP_INSTALL_RPATH:BOOL=ON)
+endif()
+
+ament_vendor(ogre_vendor
+  VCS_URL https://github.com/OGRECave/ogre.git
+  VCS_VERSION v1.12.10
+  CMAKE_ARGS
+    -DOGRE_STATIC:BOOL=OFF
+    -DOGRE_INSTALL_PDB:BOOL=OFF
+    -DOGRE_BUILD_DEPENDENCIES:BOOL=OFF
+    -DOGRE_BUILD_TESTS:BOOL=OFF
+    -DOGRE_BUILD_SAMPLES:BOOL=FALSE
+    -DOGRE_INSTALL_SAMPLES:BOOL=FALSE
+    -DOGRE_INSTALL_SAMPLES_SOURCE:BOOL=FALSE
+    -DOGRE_CONFIG_THREADS:STRING=0
+    -DOGRE_RESOURCEMANAGER_STRICT:STRING=2
+    -DOGRE_BUILD_LIBS_AS_FRAMEWORKS:BOOL=OFF
+    -DOGRE_BUILD_COMPONENT_PYTHON:BOOL=FALSE
+    -DOGRE_BUILD_COMPONENT_JAVA:BOOL=FALSE
+    -DOGRE_BUILD_COMPONENT_CSHARP:BOOL=FALSE
+    -DOGRE_BUILD_COMPONENT_BITES:BOOL=FALSE
+    -DOGRE_BUILD_PLUGIN_DOT_SCENE:BOOL=FALSE
+    -DOGRE_BUILD_TOOLS:BOOL=FALSE
+    -DOGRE_CONFIG_ENABLE_ZIP:BOOL=ON
+    -DOGRE_BUILD_RENDERSYSTEM_GL:BOOL=TRUE
+    -DOGRE_BUILD_RENDERSYSTEM_D3D11:BOOL=OFF
+    -DOGRE_BUILD_RENDERSYSTEM_D3D9:BOOL=OFF
+    -DCMAKE_POLICY_DEFAULT_CMP0074=NEW
+    "-DCMAKE_CXX_FLAGS=${OGRE_CXX_FLAGS}"
+    ${OGRE_CMAKE_ARGS}
+  PATCHES patches
+)
+
+if(TARGET ogre_vendor)
+  if(TARGET freetype_vendor)
+    ExternalProject_Add_StepDependencies(ogre_vendor configure freetype_vendor)
+  endif()
+
+  if(TARGET zlib_vendor)
+    ExternalProject_Add_StepDependencies(ogre_vendor configure zlib_vendor)
+  endif()
+endif()
 
 if(BUILD_TESTING)
   find_package(ament_lint_auto REQUIRED)
@@ -251,5 +113,5 @@ if(BUILD_TESTING)
 endif()
 
 ament_package(
-  CONFIG_EXTRAS "rviz_ogre_vendor-extras.cmake.in"
+  CONFIG_EXTRAS_POST "rviz_ogre_vendor-extras.cmake.in"
 )

rviz_assimp_vendor/CMakeLists.txt
@@ -1,67 +1,8 @@
-# lint_cmake: -readability/wonkycase
-cmake_minimum_required(VERSION 3.5)
-
+cmake_minimum_required(VERSION 3.10)
 project(rviz_assimp_vendor)
 
-option(FORCE_BUILD_VENDOR_PKG
-  "Build assimp from source, even if system-installed package is available"
-  OFF)
-
 find_package(ament_cmake REQUIRED)
-
-# Avoid DOWNLOAD_EXTRACT_TIMESTAMP warning for CMake >= 3.24
-if(POLICY CMP0135)
-  cmake_policy(SET CMP0135 NEW)
-endif()
-
-macro(build_assimp)
-  set(extra_cmake_args)
-  if(DEFINED CMAKE_BUILD_TYPE)
-    list(APPEND extra_cmake_args -DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE})
-  endif()
-  if(WIN32)
-    # the /EHsc flag is needed to prevent warnings within the assimp build
-    list(APPEND extra_cmake_args "-DCMAKE_CXX_FLAGS=/w /EHsc")
-    list(APPEND extra_cmake_args "-DCMAKE_C_FLAGS=/w")
-  else()
-    list(APPEND extra_cmake_args "-DCMAKE_CXX_FLAGS=${CMAKE_CXX_FLAGS} -std=c++14 -w")
-    list(APPEND extra_cmake_args "-DCMAKE_C_FLAGS=${CMAKE_C_FLAGS} -w")
-  endif()
-
-  list(APPEND extra_cmake_args "-DASSIMP_BUILD_ASSIMP_TOOLS=OFF")
-  list(APPEND extra_cmake_args "-DASSIMP_BUILD_TESTS=OFF")
-  list(APPEND extra_cmake_args "-DASSIMP_BUILD_SAMPLES=OFF")
-  list(APPEND extra_cmake_args "-DCMAKE_C_COMPILER=${CMAKE_C_COMPILER}")
-  list(APPEND extra_cmake_args "-DCMAKE_CXX_COMPILER=${CMAKE_CXX_COMPILER}")
-
-  if(WIN32 AND NOT ${CMAKE_VERBOSE_MAKEFILE})
-    set(should_log ON)  # prevent warnings in Windows CI
-  else()
-    set(should_log OFF)
-  endif()
-
-  include(ExternalProject)
-  ExternalProject_Add(assimp-4.1.0
-    URL https://github.com/assimp/assimp/archive/v4.1.0.tar.gz
-    URL_MD5 83b53a10c38d964bd1e69da0606e2727
-    TIMEOUT 600
-    LOG_CONFIGURE ${should_log}
-    LOG_BUILD ${should_log}
-    CMAKE_ARGS
-      -DCMAKE_INSTALL_PREFIX=${CMAKE_CURRENT_BINARY_DIR}/assimp_install
-      ${extra_cmake_args}
-      -Wno-dev
-  )
-
-  # The external project will install to the build folder, but we'll install that on make install.
-  install(
-    DIRECTORY
-      ${CMAKE_CURRENT_BINARY_DIR}/assimp_install/
-    DESTINATION
-      ${CMAKE_INSTALL_PREFIX}/opt/rviz_assimp_vendor
-    USE_SOURCE_PERMISSIONS
-  )
-endmacro()
+find_package(ament_cmake_vendor_package REQUIRED)
 
 # Override ON so that the following CMake logic in assimp 5.0.1 and older
 # doesn't result in a CMake warning: if(ON)
@@ -69,26 +10,34 @@ endmacro()
 #   https://bugs.launchpad.net/ubuntu/+source/assimp/+bug/1869405
 set(ON 1)
 
+# TODO: Switch to version range in find_package in CMake 3.19
 find_package(assimp QUIET)
-
-if(FORCE_BUILD_VENDOR_PKG OR NOT assimp_FOUND OR "${assimp_VERSION}" VERSION_LESS 4.1.0)
-  build_assimp()
-
-  if(WIN32)
-    ament_environment_hooks(env_hook/rviz_assimp_vendor_library_path.bat)
-    set(ENV_VAR_NAME "PATH")
-    set(ENV_VAR_VALUE "opt\\rviz_assimp_vendor\\bin")
-  else()
-    ament_environment_hooks(env_hook/rviz_assimp_vendor_library_path.sh)
-    if(APPLE)
-      set(ENV_VAR_NAME "DYLD_LIBRARY_PATH")
-    else()
-      set(ENV_VAR_NAME "LD_LIBRARY_PATH")
-    endif()
-    set(ENV_VAR_VALUE "opt/rviz_assimp_vendor/lib")
-  endif()
-  ament_environment_hooks(env_hook/rviz_assimp_vendor_library_path.dsv.in)
+if(NOT assimp_FOUND OR "${assimp_VERSION}" VERSION_LESS 4.1.0)
+  set(assimp_FOUND FALSE)
 endif()
+
+if(MSVC)
+  set(ASSIMP_CXX_FLAGS "/EHsc ${CMAKE_CXX_FLAGS}")
+  set(ASSIMP_CXX_FLAGS "/wd4700 ${ASSIMP_CXX_FLAGS}")
+else()
+  set(ASSIMP_CXX_FLAGS "-std=c++14 ${CMAKE_CXX_FLAGS}")
+  set(ASSIMP_CXX_FLAGS "-Wno-address-of-packed-member ${ASSIMP_CXX_FLAGS}")
+  set(ASSIMP_CXX_FLAGS "-Wno-catch-value ${ASSIMP_CXX_FLAGS}")
+  set(ASSIMP_CXX_FLAGS "-Wno-class-memaccess ${ASSIMP_CXX_FLAGS}")
+  set(ASSIMP_CXX_FLAGS "-Wno-range-loop-construct ${ASSIMP_CXX_FLAGS}")
+  set(ASSIMP_CXX_FLAGS "-Wno-tautological-compare ${ASSIMP_CXX_FLAGS}")
+endif()
+
+ament_vendor(assimp_vendor
+  SATISFIED ${assimp_FOUND}
+  VCS_URL https://github.com/assimp/assimp.git
+  VCS_VERSION v4.1.0
+  CMAKE_ARGS
+    -DASSIMP_BUILD_ASSIMP_TOOLS:BOOL=OFF
+    -DASSIMP_BUILD_TESTS:BOOL=OFF
+    -DASSIMP_BUILD_SAMPLES:BOOL=OFF
+    "-DCMAKE_CXX_FLAGS=${ASSIMP_CXX_FLAGS}"
+)
 
 if(BUILD_TESTING)
   find_package(ament_lint_auto REQUIRED)

rviz_default_plugins/CMakeLists.txt
@@ -61,6 +61,7 @@ find_package(Qt5 REQUIRED COMPONENTS Widgets Test)
 
 find_package(geometry_msgs REQUIRED)
 
+find_package(ignition_math6_vendor REQUIRED)
 find_package(ignition-math6 REQUIRED)
 
 find_package(image_transport REQUIRED)

rviz_assimp_vendor/env_hook/rviz_assimp_vendor_library_path.bat
@@ -1,41 +1,0 @@
-:: copied from rviz_assimp_vendor/env_hook/rviz_assimp_vendor_library_path.bat
-@echo off
-
-call:ament_prepend_unique_value PATH "%AMENT_CURRENT_PREFIX%\opt\rviz_assimp_vendor\bin"
-
-goto:eof
-
-:: Prepend non-duplicate values to environment variables
-:: using semicolons as separators and avoiding trailing separators.
-:: first argument: the name of the result variable
-:: second argument: the value
-:ament_prepend_unique_value
-  setlocal enabledelayedexpansion
-  :: arguments
-  set "listname=%~1"
-  set "value=%~2"
-  :: skip if path doesn't exist
-  if NOT EXIST "%value%" (
-    goto:eof
-  )
-  :: expand the list variable
-  set "list=!%listname%!"
-  :: check if the list contains the value
-  set "is_duplicate="
-  if "%list%" NEQ "" (
-    for %%v in ("%list:;=";"%") do (
-      if "%%~v" == "%value%" set "is_duplicate=1"
-    )
-  )
-  :: if it is not a duplicate prepend it
-  if "%is_duplicate%" == "" (
-    :: if not empty, prepend a semi-colon
-    if "!list!" NEQ "" set "list=;!list!"
-    :: prepend the value
-    set "list=%value%!list!"
-  )
-  endlocal & (
-    :: set result variable in parent scope
-    set "%~1=%list%"
-  )
-goto:eof

rviz_assimp_vendor/env_hook/rviz_assimp_vendor_library_path.dsv.in
@@ -1,1 +1,0 @@
-prepend-non-duplicate;@ENV_VAR_NAME@;@ENV_VAR_VALUE@

rviz_assimp_vendor/env_hook/rviz_assimp_vendor_library_path.sh
@@ -1,16 +1,0 @@
-# copied from rviz_assimp_vendor/env_hook/rviz_assimp_vendor_library_path.sh
-
-# detect if running on Darwin platform
-_UNAME=`uname -s`
-_IS_DARWIN=0
-if [ "$_UNAME" = "Darwin" ]; then
-  _IS_DARWIN=1
-fi
-unset _UNAME
-
-if [ $_IS_DARWIN -eq 0 ]; then
-  ament_prepend_unique_value LD_LIBRARY_PATH "$AMENT_CURRENT_PREFIX/opt/rviz_assimp_vendor/lib"
-else
-  ament_prepend_unique_value DYLD_LIBRARY_PATH "$AMENT_CURRENT_PREFIX/opt/rviz_assimp_vendor/lib"
-fi
-unset _IS_DARWIN

rviz_assimp_vendor/package.xml
@@ -22,6 +22,7 @@
   <author email="william@openrobotics.org">William Woodall</author>
 
   <buildtool_depend>ament_cmake</buildtool_depend>
+  <buildtool_depend>ament_cmake_vendor_package</buildtool_depend>
 
   <depend>assimp</depend>
 

rviz_ogre_vendor/env_hook/rviz_ogre_vendor_library_path.bat
@@ -1,41 +1,0 @@
-:: copied from rviz_ogre_vendor/env_hook/rviz_ogre_vendor_library_path.bat
-@echo off
-
-call:ament_prepend_unique_value PATH "%AMENT_CURRENT_PREFIX%\opt\rviz_ogre_vendor\bin"
-
-goto:eof
-
-:: Prepend non-duplicate values to environment variables
-:: using semicolons as separators and avoiding trailing separators.
-:: first argument: the name of the result variable
-:: second argument: the value
-:ament_prepend_unique_value
-  setlocal enabledelayedexpansion
-  :: arguments
-  set "listname=%~1"
-  set "value=%~2"
-  :: skip if path doesn't exist
-  if NOT EXIST "%value%" (
-    goto:eof
-  )
-  :: expand the list variable
-  set "list=!%listname%!"
-  :: check if the list contains the value
-  set "is_duplicate="
-  if "%list%" NEQ "" (
-    for %%v in ("%list:;=";"%") do (
-      if "%%~v" == "%value%" set "is_duplicate=1"
-    )
-  )
-  :: if it is not a duplicate prepend it
-  if "%is_duplicate%" == "" (
-    :: if not empty, prepend a semi-colon
-    if "!list!" NEQ "" set "list=;!list!"
-    :: prepend the value
-    set "list=%value%!list!"
-  )
-  endlocal & (
-    :: set result variable in parent scope
-    set "%~1=%list%"
-  )
-goto:eof

commit 0c1d9c4f7df568fe8955f14008f95adb87560731

Use custom FindFreetype durin ogre build
Fixes: 4e06de1cf277
Signed-off-by: Scott K Logan <logans@cottsay.net>

rviz_ogre_vendor/CMakeLists.txt
@@ -67,6 +67,8 @@ if(NOT WIN32)
   list(APPEND OGRE_CMAKE_ARGS -DCMAKE_SKIP_INSTALL_RPATH:BOOL=ON)
 endif()
 
+list(INSERT CMAKE_MODULE_PATH 0 "${CMAKE_CURRENT_SOURCE_DIR}")
+
 ament_vendor(ogre_vendor
   VCS_URL https://github.com/OGRECave/ogre.git
   VCS_VERSION v1.12.10
@@ -93,6 +95,7 @@ ament_vendor(ogre_vendor
     -DOGRE_BUILD_RENDERSYSTEM_D3D9:BOOL=OFF
     -DCMAKE_POLICY_DEFAULT_CMP0074=NEW
     "-DCMAKE_CXX_FLAGS=${OGRE_CXX_FLAGS}"
+    "-DCMAKE_MODULE_PATH=${CMAKE_MODULE_PATH}"
     ${OGRE_CMAKE_ARGS}
   PATCHES patches
 )

rviz_ogre_vendor/FindFreetype.cmake
@@ -1,61 +1,0 @@
-#-------------------------------------------------------------------
-# This file is part of the CMake build system for OGRE
-#     (Object-oriented Graphics Rendering Engine)
-# For the latest info, see http://www.ogre3d.org/
-#
-# The contents of this file are placed in the public domain. Feel
-# free to make use of it in any way you like.
-#-------------------------------------------------------------------
-
-# - Try to find FreeType
-# Once done, this will define
-#
-#  FREETYPE_FOUND - system has FreeType
-#  FREETYPE_INCLUDE_DIRS - the FreeType include directories 
-#  FREETYPE_LIBRARIES - link these to use FreeType
-
-include(FindPkgMacros)
-findpkg_begin(FREETYPE)
-
-# Get path, convert backslashes as ${ENV_${var}}
-getenv_path(FREETYPE_HOME)
-
-# construct search paths
-set(FREETYPE_PREFIX_PATH ${FREETYPE_HOME} ${ENV_FREETYPE_HOME})
-create_search_paths(FREETYPE)
-# redo search if prefix path changed
-clear_if_changed(FREETYPE_PREFIX_PATH
-  FREETYPE_LIBRARY_FWK
-  FREETYPE_LIBRARY_REL
-  FREETYPE_LIBRARY_DBG
-  FREETYPE_INCLUDE_DIR
-)
-
-set(FREETYPE_LIBRARY_NAMES freetype253 freetype252 freetype251 freetype2501 freetype250 freetype2412 freetype2411 freetype2410 freetype249 freetype248 freetype246 freetype2311 freetype239 freetype238 freetype235 freetype219 freetype)
-get_debug_names(FREETYPE_LIBRARY_NAMES)
-
-use_pkgconfig(FREETYPE_PKGC freetype2)
-
-# prefer static library over framework 
-set(CMAKE_FIND_FRAMEWORK "LAST")
-
-message(STATUS "CMAKE_PREFIX_PATH: ${CMAKE_PREFIX_PATH}")
-findpkg_framework(FREETYPE)
-message(STATUS "CMAKE_PREFIX_PATH: ${CMAKE_PREFIX_PATH}")
-
-find_path(FREETYPE_INCLUDE_DIR NAMES freetype.h freetype/freetype.h HINTS ${FREETYPE_INC_SEARCH_PATH} ${FREETYPE_PKGC_INCLUDE_DIRS} PATH_SUFFIXES freetype2)
-find_path(FREETYPE_FT2BUILD_INCLUDE_DIR NAMES ft2build.h HINTS ${FREETYPE_INC_SEARCH_PATH} ${FREETYPE_PKGC_INCLUDE_DIRS} PATH_SUFFIXES freetype2)
-
-find_library(FREETYPE_LIBRARY_REL NAMES ${FREETYPE_LIBRARY_NAMES} HINTS ${FREETYPE_LIB_SEARCH_PATH} ${FREETYPE_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" Release RelWithDebInfo MinSizeRel)
-find_library(FREETYPE_LIBRARY_DBG NAMES ${FREETYPE_LIBRARY_NAMES_DBG} HINTS ${FREETYPE_LIB_SEARCH_PATH} ${FREETYPE_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" Debug)
-
-make_library_set(FREETYPE_LIBRARY)
-
-findpkg_finish(FREETYPE)
-mark_as_advanced(FREETYPE_FT2BUILD_INCLUDE_DIR)
-if (NOT FREETYPE_FT2BUILD_INCLUDE_DIR STREQUAL FREETYPE_INCLUDE_DIR)
-  set(FREETYPE_INCLUDE_DIRS ${FREETYPE_INCLUDE_DIRS} ${FREETYPE_FT2BUILD_INCLUDE_DIR})
-endif ()
-
-# Reset framework finding
-set(CMAKE_FIND_FRAMEWORK "FIRST")

rviz_ogre_vendor/patches/0005-find-freetype.patch
@@ -1,0 +1,67 @@
+diff --git a/CMake/Packages/FindFreetype.cmake b/CMake/Packages/FindFreetype.cmake
+new file mode 100644
+index 000000000000..d2017f6ba6dd
+--- /dev/null
++++ b/CMake/Packages/FindFreetype.cmake
+@@ -0,0 +1,61 @@
++#-------------------------------------------------------------------
++# This file is part of the CMake build system for OGRE
++#     (Object-oriented Graphics Rendering Engine)
++# For the latest info, see http://www.ogre3d.org/
++#
++# The contents of this file are placed in the public domain. Feel
++# free to make use of it in any way you like.
++#-------------------------------------------------------------------
++
++# - Try to find FreeType
++# Once done, this will define
++#
++#  FREETYPE_FOUND - system has FreeType
++#  FREETYPE_INCLUDE_DIRS - the FreeType include directories 
++#  FREETYPE_LIBRARIES - link these to use FreeType
++
++include(FindPkgMacros)
++findpkg_begin(FREETYPE)
++
++# Get path, convert backslashes as ${ENV_${var}}
++getenv_path(FREETYPE_HOME)
++
++# construct search paths
++set(FREETYPE_PREFIX_PATH ${FREETYPE_HOME} ${ENV_FREETYPE_HOME})
++create_search_paths(FREETYPE)
++# redo search if prefix path changed
++clear_if_changed(FREETYPE_PREFIX_PATH
++  FREETYPE_LIBRARY_FWK
++  FREETYPE_LIBRARY_REL
++  FREETYPE_LIBRARY_DBG
++  FREETYPE_INCLUDE_DIR
++)
++
++set(FREETYPE_LIBRARY_NAMES freetype253 freetype252 freetype251 freetype2501 freetype250 freetype2412 freetype2411 freetype2410 freetype249 freetype248 freetype246 freetype2311 freetype239 freetype238 freetype235 freetype219 freetype)
++get_debug_names(FREETYPE_LIBRARY_NAMES)
++
++use_pkgconfig(FREETYPE_PKGC freetype2)
++
++# prefer static library over framework 
++set(CMAKE_FIND_FRAMEWORK "LAST")
++
++message(STATUS "CMAKE_PREFIX_PATH: ${CMAKE_PREFIX_PATH}")
++findpkg_framework(FREETYPE)
++message(STATUS "CMAKE_PREFIX_PATH: ${CMAKE_PREFIX_PATH}")
++
++find_path(FREETYPE_INCLUDE_DIR NAMES freetype.h freetype/freetype.h HINTS ${FREETYPE_INC_SEARCH_PATH} ${FREETYPE_PKGC_INCLUDE_DIRS} PATH_SUFFIXES freetype2)
++find_path(FREETYPE_FT2BUILD_INCLUDE_DIR NAMES ft2build.h HINTS ${FREETYPE_INC_SEARCH_PATH} ${FREETYPE_PKGC_INCLUDE_DIRS} PATH_SUFFIXES freetype2)
++
++find_library(FREETYPE_LIBRARY_REL NAMES ${FREETYPE_LIBRARY_NAMES} HINTS ${FREETYPE_LIB_SEARCH_PATH} ${FREETYPE_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" Release RelWithDebInfo MinSizeRel)
++find_library(FREETYPE_LIBRARY_DBG NAMES ${FREETYPE_LIBRARY_NAMES_DBG} HINTS ${FREETYPE_LIB_SEARCH_PATH} ${FREETYPE_PKGC_LIBRARY_DIRS} PATH_SUFFIXES "" Debug)
++
++make_library_set(FREETYPE_LIBRARY)
++
++findpkg_finish(FREETYPE)
++mark_as_advanced(FREETYPE_FT2BUILD_INCLUDE_DIR)
++if (NOT FREETYPE_FT2BUILD_INCLUDE_DIR STREQUAL FREETYPE_INCLUDE_DIR)
++  set(FREETYPE_INCLUDE_DIRS ${FREETYPE_INCLUDE_DIRS} ${FREETYPE_FT2BUILD_INCLUDE_DIR})
++endif ()
++
++# Reset framework finding
++set(CMAKE_FIND_FRAMEWORK "FIRST")