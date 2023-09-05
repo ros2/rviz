efbb44719e09498639bb7c495f87a0816a2b80c1
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2023-09-05 22:07:22 +0200

    Improve error handling in LaserScanDisplay (#1035)
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- rviz_default_plugins/src/rviz_default_plugins/displays/laser_scan/laser_scan_display.cpp
+++ rviz_default_plugins/src/rviz_default_plugins/displays/laser_scan/laser_scan_display.cpp
@@ -35,10 +35,13 @@
 #include "tf2_ros/buffer.h"
 
 #include "rviz_common/properties/int_property.hpp"
+#include "rviz_common/properties/status_property.hpp"
 #include "rviz_common/transformation/transformation_manager.hpp"
 #include "rviz_common/validate_floats.hpp"
 #include "rviz_default_plugins/displays/pointcloud/point_cloud_common.hpp"
 #include "rviz_default_plugins/transformation/tf_wrapper.hpp"
+
+#include <rclcpp/duration.hpp>
 
 namespace rviz_default_plugins
 {
@@ -61,6 +64,19 @@ void LaserScanDisplay::onInitialize()
   transformer_guard_->initialize(context_);
 }
 
+void LaserScanDisplay::checkTolerance(rclcpp::Duration tolerance)
+{
+  auto tolerance_seg = RCL_NS_TO_S(tolerance.nanoseconds());
+  if (tolerance_seg > 1) {
+    setStatus(
+      rviz_common::properties::StatusProperty::Warn, "Scan Time",
+      QString(
+        "Laser scan time, computed from time_increment * len(ranges), is rather large: "
+        " %1s.\nThe display of any message will be delayed by this amount of time!")
+      .arg(tolerance_seg));
+  }
+}
+
 void LaserScanDisplay::processMessage(sensor_msgs::msg::LaserScan::ConstSharedPtr scan)
 {
 //  Compute tolerance necessary for this scan
@@ -72,6 +88,7 @@ void LaserScanDisplay::processMessage(sensor_msgs::msg::LaserScan::ConstSharedPt
   if (tolerance > filter_tolerance_) {
     filter_tolerance_ = tolerance;
     tf_filter_->setTolerance(filter_tolerance_);
+    checkTolerance(filter_tolerance_);
   }
   auto cloud = std::make_shared<sensor_msgs::msg::PointCloud2>();
   auto tf_wrapper = std::dynamic_pointer_cast<transformation::TFWrapper>(
@@ -108,6 +125,7 @@ void LaserScanDisplay::reset()
 {
   MFDClass::reset();
   point_cloud_common_->reset();
+  checkTolerance(filter_tolerance_);
 }
 
 void LaserScanDisplay::onDisable()
