a07eb9dddbef884e1395e789337c9eb0572f5429
Markus Bader <markus.bader@mx-robotics.com>
2023-10-13 15:14:17 +0200

    defaut title restored and {} on title variables set

--- rviz_common/src/rviz_common/visualization_frame.cpp
+++ rviz_common/src/rviz_common/visualization_frame.cpp
@@ -755,29 +755,22 @@ void VisualizationFrame::setDisplayConfigFile(const std::string & path)
     } else {
       title = QDir::toNativeSeparators(QString::fromStdString(path)).toStdString() + "[*] - RViz";
     }
-
-    /// Adding a namespace prefix on the window frame title
-    std::string node_namespace;
-    node_namespace = rviz_ros_node_.lock()->get_raw_node()->get_namespace();
-    if (node_namespace.compare("/") != 0) {
-      title = node_namespace + " - " + title;
-    }
   } else {
     title = display_title_format_;
     std::filesystem::path full_filename(path.c_str());
     std::size_t found;
-    found = title.find("NAMESPACE");
+    found = title.find("{NAMESPACE}");
     if (found != std::string::npos) {
       title.replace(
-        found, sizeof("NAMESPACE") - 1, rviz_ros_node_.lock()->get_raw_node()->get_namespace());
+        found, sizeof("{NAMESPACE}") - 1, rviz_ros_node_.lock()->get_raw_node()->get_namespace());
     }
-    found = title.find("PATH");
+    found = title.find("{PATH}");
     if (found != std::string::npos) {
-      title.replace(found, sizeof("PATH") - 1, full_filename.relative_path());
+      title.replace(found, sizeof("{PATH}") - 1, full_filename.relative_path());
     }
-    found = title.find("FILE");
+    found = title.find("{FILE}");
     if (found != std::string::npos) {
-      title.replace(found, sizeof("FILE") - 1, full_filename.filename());
+      title.replace(found, sizeof("{FILE}") - 1, full_filename.filename());
     }
     found = title.find("[*]");
     if (found == std::string::npos) {
