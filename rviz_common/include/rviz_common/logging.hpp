e9b4435935d52dbff484380cdb2a7af9d4b4e75c
Chris Lalancette <clalancette@gmail.com>
2024-02-27 13:26:11 -0500

    Fixes for uncrustify 0.78. (#1155)
    Mostly what we do here is to disable the indentation on
    certain constructs that are different between 0.72 and
    0.78.  It isn't my preferred solution, but since it only
    affects a small amount of code (and most of that in macros),
    this seems acceptable to me.
    
    Signed-off-by: Chris Lalancette <clalancette@gmail.com>

--- rviz_common/include/rviz_common/logging.hpp
+++ rviz_common/include/rviz_common/logging.hpp
@@ -59,45 +59,49 @@
 #include "rviz_rendering/logging_handler.hpp"
 #include "rviz_common/visibility_control.hpp"
 
+// *INDENT-OFF*
+
 #define RVIZ_COMMON_LOG_DEBUG(msg) do { \
-    rviz_common::log_debug(msg, __FILE__, __LINE__); \
+  rviz_common::log_debug(msg, __FILE__, __LINE__); \
 } while (0)
 
 #define RVIZ_COMMON_LOG_DEBUG_STREAM(args) do { \
-    std::stringstream __ss; \
-    __ss << args; \
-    rviz_common::log_debug(__ss.str(), __FILE__, __LINE__); \
+  std::stringstream __ss; \
+  __ss << args; \
+  rviz_common::log_debug(__ss.str(), __FILE__, __LINE__); \
 } while (0)
 
 #define RVIZ_COMMON_LOG_INFO(msg) do { \
-    rviz_common::log_info(msg, __FILE__, __LINE__); \
+  rviz_common::log_info(msg, __FILE__, __LINE__); \
 } while (0)
 
 #define RVIZ_COMMON_LOG_INFO_STREAM(args) do { \
-    std::stringstream __ss; \
-    __ss << args; \
-    rviz_common::log_info(__ss.str(), __FILE__, __LINE__); \
+  std::stringstream __ss; \
+  __ss << args; \
+  rviz_common::log_info(__ss.str(), __FILE__, __LINE__); \
 } while (0)
 
 #define RVIZ_COMMON_LOG_WARNING(msg) do { \
-    rviz_common::log_warning(msg, __FILE__, __LINE__); \
+  rviz_common::log_warning(msg, __FILE__, __LINE__); \
 } while (0)
 
 #define RVIZ_COMMON_LOG_WARNING_STREAM(args) do { \
-    std::stringstream __ss; \
-    __ss << args; \
-    rviz_common::log_warning(__ss.str(), __FILE__, __LINE__); \
+  std::stringstream __ss; \
+  __ss << args; \
+  rviz_common::log_warning(__ss.str(), __FILE__, __LINE__); \
 } while (0)
 
 #define RVIZ_COMMON_LOG_ERROR(msg) do { \
-    rviz_common::log_error(msg, __FILE__, __LINE__); \
+  rviz_common::log_error(msg, __FILE__, __LINE__); \
 } while (0)
 
 #define RVIZ_COMMON_LOG_ERROR_STREAM(args) do { \
-    std::stringstream __ss; \
-    __ss << args; \
-    rviz_common::log_error(__ss.str(), __FILE__, __LINE__); \
+  std::stringstream __ss; \
+  __ss << args; \
+  rviz_common::log_error(__ss.str(), __FILE__, __LINE__); \
 } while (0)
+
+// *INDENT-ON*
 
 namespace rviz_common
 {
