dd07c09dfcd87bd599e5a574604402d5bfd709ac
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-03-14 10:45:34 +0100

    Added trianglePolygon test
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- rviz_rendering/test/rviz_rendering/objects/triangle_polygon_test.cpp
+++ rviz_rendering/test/rviz_rendering/objects/triangle_polygon_test.cpp
@@ -29,40 +29,50 @@
  * POSSIBILITY OF SUCH DAMAGE.
  */
 
-#ifndef RVIZ_RENDERING__OBJECTS__TRIANGLE_POLYGON_HPP_
-#define RVIZ_RENDERING__OBJECTS__TRIANGLE_POLYGON_HPP_
+#include <gtest/gtest.h>  // NOLINT
+#include <gmock/gmock.h>  // NOLINT
 
-#include <string>
+#include <memory>
+#include <vector>
 
-#include <OgreColourValue.h>
-#include <OgreManualObject.h>
-#include <OgreSceneNode.h>
-#include <OgreSceneManager.h>
-#include <OgreVector.h>
+#include <Ogre.h>
 
-namespace rviz_rendering
+#include "rviz_rendering/objects/triangle_polygon.hpp"
+#include "../ogre_testing_environment.hpp"
+
+#include "../matcher.hpp"
+
+class TrianglePolygonTestFixture : public ::testing::Test
 {
-class TrianglePolygon
-{
-public:
-  TrianglePolygon(
-    Ogre::SceneManager * manager,
-    Ogre::SceneNode * node,
-    const Ogre::Vector3 & O,
-    const Ogre::Vector3 & A,
-    const Ogre::Vector3 & B,
-    const std::string & name,
-    const Ogre::ColourValue & color,
-    bool use_color,
-    bool upper_triangle);
-  virtual ~TrianglePolygon();
-
-  Ogre::ManualObject * getManualObject();
-
 protected:
-  Ogre::ManualObject * manual_;
-  Ogre::SceneManager * manager_;
+  void SetUp()
+  {
+    testing_environment_ = std::make_shared<rviz_rendering::OgreTestingEnvironment>();
+    testing_environment_->setUpOgreTestEnvironment();
+  }
+
+  std::shared_ptr<rviz_rendering::OgreTestingEnvironment> testing_environment_;
 };
 
-}  // namespace rviz_rendering
-#endif  // RVIZ_RENDERING__OBJECTS__TRIANGLE_POLYGON_HPP_
+TEST_F(TrianglePolygonTestFixture, setPoints_sets_the_line_position_and_size) {
+  auto scene_manager = Ogre::Root::getSingletonPtr()->createSceneManager();
+  auto root_node = scene_manager->getRootSceneNode();
+
+  auto O = Ogre::Vector3(0, 0, 0);
+  auto A = Ogre::Vector3(0, 1, 0);
+  auto B = Ogre::Vector3(1, 0.5, 0);
+  auto color = Ogre::ColourValue(255, 0, 0, 1.0);
+  auto triangle_polygon = new rviz_rendering::TrianglePolygon(
+    scene_manager, root_node, O, A, B, "test_triangle",
+    color, false, false);
+
+  auto manual_object = triangle_polygon->getManualObject();
+
+
+  // auto triangle_node = dynamic_cast<Ogre::SceneNode *>(root_node->getChild(0));
+  auto aabb = manual_object->getBoundingBox();
+
+  ASSERT_THAT(aabb.getMinimum(), Vector3Eq(Ogre::Vector3(0, 0, 0)));
+  ASSERT_THAT(aabb.getMaximum(), Vector3Eq(Ogre::Vector3(1, 1, 0)));
+  delete triangle_polygon;
+}
