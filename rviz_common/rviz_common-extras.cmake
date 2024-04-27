d7c888865777e657400341202a1d964e317fb224
wep21 <daisuke.nishimatsu1021@gmail.com>
2024-04-27 13:22:17 +0900

    workaround for windows ci
    Signed-off-by: wep21 <daisuke.nishimatsu1021@gmail.com>

--- rviz_common/rviz_common-extras.cmake
+++ rviz_common/rviz_common-extras.cmake
@@ -30,10 +30,10 @@
 
 # find package Qt5 because otherwise using the rviz_common::rviz_common
 # exported target will complain that the Qt5::Widgets target does not exist
-if(DEFINED ENV{Qt6_DIR})
-  set(QT_DIR $ENV{Qt6_DIR})
-elseif(DEFINED ENV{Qt5_DIR})
-  set(QT_DIR $ENV{Qt5_DIR})
+if(MSVC)
+  set(QT_VERSION_MAJOR 5)
+  find_package(Qt${QT_VERSION_MAJOR} QUIET COMPONENTS Widgets)
+else()
+  find_package(QT NAMES Qt6 Qt5 QUIET COMPONENTS Widgets)
+  find_package(Qt${QT_VERSION_MAJOR} QUIET COMPONENTS Widgets)
 endif()
-find_package(QT NAMES Qt6 Qt5 REQUIRED QUIET COMPONENTS Widgets)
-find_package(Qt${QT_VERSION_MAJOR} REQUIRED QUIET COMPONENTS Widgets)
