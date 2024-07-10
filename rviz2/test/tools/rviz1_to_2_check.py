51c77259c05fa298de8c50fb1e6a620813668e28
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-07-10 15:02:49 +0200

    Fixed RViz2 linters
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- rviz2/test/tools/rviz1_to_2_check.py
+++ rviz2/test/tools/rviz1_to_2_check.py
@@ -1,4 +1,32 @@
 #!/usr/bin/env python3
+# Copyright 2024 Open Source Robotics Foundation, Inc.
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the copyright holder nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
 import os
 import subprocess
@@ -74,12 +102,12 @@ def test_convert_all_ros1():
 
     actual_stderr = result.stderr.decode()
     expected_stderr = (
-        "Cannot migrate display rviz/AccelStamped - skipping",
-        "Cannot migrate display rviz/DepthCloud - skipping",
-        "Cannot migrate display rviz/Effort - skipping",
-        "Cannot migrate display rviz/TwistStamped - skipping",
-        "Cannot migrate display rviz_plugin_tutorials/Imu - skipping",
-        "Unable to migrate view type rviz/FrameAligned - skipping")
+        'Cannot migrate display rviz/AccelStamped - skipping',
+        'Cannot migrate display rviz/DepthCloud - skipping',
+        'Cannot migrate display rviz/Effort - skipping',
+        'Cannot migrate display rviz/TwistStamped - skipping',
+        'Cannot migrate display rviz_plugin_tutorials/Imu - skipping',
+        'Unable to migrate view type rviz/FrameAligned - skipping')
 
     for expected_line, line in zip(expected_stderr, actual_stderr.split('\n')):
         # Strip line endings for Linux/Windows compatibility
