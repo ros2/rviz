a5372dcfdfa6537264e6192511696f9b58423641
Alejandro Hernández Cordero <ahcorde@gmail.com>
2024-07-10 17:17:56 +0200

    Added new line in license
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- rviz_default_plugins/test/rviz_default_plugins/publishers/pose_publisher.hpp
+++ rviz_default_plugins/test/rviz_default_plugins/publishers/pose_publisher.hpp
@@ -1,5 +1,6 @@
 // Copyright (c) 2018, Bosch Software Innovations GmbH.
 // All rights reserved.
+//
 // Redistribution and use in source and binary forms, with or without
 // modification, are permitted provided that the following conditions are met:
 //
