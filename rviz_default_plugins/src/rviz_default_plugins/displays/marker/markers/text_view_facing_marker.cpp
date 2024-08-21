a4452caae3314905e522763e6fee37b9d08969a3
Tom Moore <tmoore@locusrobotics.com>
2024-08-21 12:54:58 +0100

    Enabling manual space width for TextViewFacingMarker (#1261)
    Signed-off-by: Tom Moore <tmoore@locusrobotics.com>
    (cherry picked from commit db7e7f8e8e3e1d848c24d8430bb66e3d79a1e50f)

--- rviz_default_plugins/src/rviz_default_plugins/displays/marker/markers/text_view_facing_marker.cpp
+++ rviz_default_plugins/src/rviz_default_plugins/displays/marker/markers/text_view_facing_marker.cpp
@@ -84,6 +84,9 @@ void TextViewFacingMarker::onNewMessage(
   scene_node_->setVisible(true);
 
   setPosition(pos);
+  if (new_message->scale.x != 0.0) {
+    text_->setSpaceWidth(new_message->scale.x);
+  }
   text_->setCharacterHeight(new_message->scale.z);
   text_->setColor(
     Ogre::ColourValue(
