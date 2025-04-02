ad55c7110a2bc9851f63d85030986027ccdd4a97
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2025-04-02 21:49:07 +0200

    Update rviz_common/test/config_test.cpp
    Co-authored-by: Christophe Bedard <bedard.christophe@gmail.com>
    Signed-off-by: Alejandro Hernández Cordero <ahcorde@gmail.com>

--- rviz_common/test/config_test.cpp
+++ rviz_common/test/config_test.cpp
@@ -76,6 +76,9 @@ TEST(Config, mapGetValue_key_not_found_null_check) {
   QString s;
   EXPECT_FALSE(c.mapGetString("non_existent_key", &s));
   EXPECT_EQ(s, "");
+  QString s_default("my_default_value");
+  EXPECT_FALSE(c.mapGetString("non_existent_key", &s_default));
+  EXPECT_EQ(s_default, "my_default_value");
 }
 
 int main(int argc, char ** argv)
