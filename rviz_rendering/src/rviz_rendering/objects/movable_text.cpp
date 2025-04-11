a3fd2f547f3c4ca3cb8d326b577e72c06e46ffe7
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2025-04-11 18:14:13 +0200

    MovableText constructor does not validate invalid character height, default fallback missing (#1398)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>
    Signed-off-by: Alejandro Hernández Cordero <alejandro@openrobotics.org>

--- rviz_rendering/src/rviz_rendering/objects/movable_text.cpp
+++ rviz_rendering/src/rviz_rendering/objects/movable_text.cpp
@@ -78,7 +78,6 @@ MovableText::MovableText(
   horizontal_alignment_(H_LEFT),
   vertical_alignment_(V_BELOW),
   color_(color),
-  char_height_(charHeight),
   line_spacing_(0.01f),
   space_width_(0),
   needs_color_update_(true),
@@ -87,6 +86,7 @@ MovableText::MovableText(
   local_translation_(0.0f),
   font_(nullptr)
 {
+  char_height_ = (charHeight > 0.0f) ? charHeight : 1.0f;
   static int count = 0;
   std::stringstream ss;
   ss << "MovableText" << count++;
