d2cb7e087c4702245942f957af2fda87046557e5
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-07-09 11:29:29 +0200

    point_cloud_transport update API call
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- rviz_default_plugins/include/rviz_default_plugins/displays/pointcloud/point_cloud_transport_display.hpp
+++ rviz_default_plugins/include/rviz_default_plugins/displays/pointcloud/point_cloud_transport_display.hpp
@@ -110,15 +110,9 @@ protected:
     }
 
     try {
-      auto required_interfaces = std::make_shared<rclcpp::node_interfaces::NodeInterfaces<
-            rclcpp::node_interfaces::NodeBaseInterface,
-            rclcpp::node_interfaces::NodeParametersInterface,
-            rclcpp::node_interfaces::NodeTopicsInterface,
-            rclcpp::node_interfaces::NodeLoggingInterface>>(*rviz_ros_node_.lock()->get_raw_node());
-
       subscription_ = std::make_shared<point_cloud_transport::SubscriberFilter>();
       subscription_->subscribe(
-        required_interfaces,
+        *rviz_ros_node_.lock()->get_raw_node(),
         getPointCloud2BaseTopicFromTopic(topic_property_->getTopicStd()),
         getPointCloud2TransportFromTopic(topic_property_->getTopicStd()),
         qos_profile);
