98fb24d1cfa6a7f3a9660a627477db2ebe28d6e2
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-10-15 12:25:58 +0200

    Allow to create more than 1 3D scene
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- rviz_common/include/rviz_common/visualization_manager.hpp
+++ rviz_common/include/rviz_common/visualization_manager.hpp
@@ -68,7 +68,7 @@ class Property;
 class PropertyTreeModel;
 class StatusList;
 class TfFrameProperty;
-
+class DisplayGroupVisibilityProperty;
 }  // namespace properties
 
 class Display;
@@ -294,6 +294,9 @@ public:
 
   rclcpp::Clock::SharedPtr getClock() override;
 
+  int addRenderPanel(RenderPanel* rp);
+  void updateRenderMask(int id, bool mask);
+
 public Q_SLOTS:
   /// Resets the wall and ROS elapsed time to zero and calls resetDisplays().
   void resetTime();
@@ -360,6 +363,7 @@ protected:
   properties::TfFrameProperty * fixed_frame_property_;
   properties::StatusList * global_status_;
   properties::IntProperty * fps_property_;
+  properties::DisplayGroupVisibilityProperty * visibility_property_;
 
   RenderPanel * render_panel_;
 
@@ -390,9 +394,13 @@ protected:
   rclcpp::Clock::SharedPtr clock_;
   rclcpp::JumpHandler::SharedPtr clock_jump_handler_;
 
+  std::vector<RenderPanel*> render_panel_list_;
+  std::vector<bool> render_panel_render_mask_;
+
 private Q_SLOTS:
   void updateFixedFrame();
   void updateBackgroundColor();
+  void updateVisibilityMask();
   void updateFps();
 
 private:
--- rviz_common/src/rviz_common/visualization_manager.cpp
+++ rviz_common/src/rviz_common/visualization_manager.cpp
@@ -69,6 +69,7 @@
 #include "frame_manager.hpp"
 #include "rviz_common/load_resource.hpp"
 #include "rviz_common/properties/color_property.hpp"
+#include "rviz_common/properties/display_group_visibility_property.hpp"
 #include "rviz_common/properties/int_property.hpp"
 #include "rviz_common/properties/parse_color.hpp"
 #include "rviz_common/properties/property.hpp"
@@ -199,12 +200,22 @@ VisualizationManager::VisualizationManager(
     "RViz will try to render this many frames per second.",
     global_options_, SLOT(updateFps()), this);
 
+  rviz_rendering::RenderWindowOgreAdapter::setVisibilityMask(getRenderPanel()->getRenderWindow(), default_visibility_bit_);
+
+  visibility_property_ = new rviz_common::properties::DisplayGroupVisibilityProperty(
+    default_visibility_bit_, getRootDisplayGroup(), nullptr, "Visibility", true,
+    "Changes the visibility of other Displays in the camera view.", global_options_,
+    SLOT(updateVisibilityMask()), this);
+  visibility_property_->setIcon(
+    rviz_common::loadPixmap("package://rviz_default_plugins/icons/visibility.svg", true));
+
   root_display_group_->initialize(this);   // only initialize() a Display
                                            // after its sub-properties are created.
   root_display_group_->setEnabled(true);
 
   updateFixedFrame();
   updateBackgroundColor();
+  updateVisibilityMask();
 
   global_status_ = new StatusList("Global Status", root_display_group_);
   global_status_->setReadOnly(true);
@@ -396,6 +407,16 @@ void VisualizationManager::onUpdate()
     render_requested_ = 0;
     std::lock_guard<std::mutex> lock(private_->render_mutex_);
     ogre_root_->renderOneFrame();
+
+    for (unsigned int i = 0; i < render_panel_list_.size(); i++) {
+      if(render_panel_render_mask_[i]) {
+        auto viewport =
+          rviz_rendering::RenderWindowOgreAdapter::getOgreViewport(render_panel_list_[i]->getRenderWindow());
+        if (viewport) {
+          viewport->update();
+        }
+      }
+    }
   }
 }
 
@@ -410,6 +431,20 @@ void VisualizationManager::onTimeJump(const rcl_time_jump_t & jump)
     RVIZ_COMMON_LOG_WARNING_STREAM(
       "Detected jump back in time. Resetting RViz.");
     Q_EMIT timeJumped();
+  }
+}
+
+int VisualizationManager::addRenderPanel( RenderPanel* rp )
+{
+  render_panel_list_.push_back(rp);
+  render_panel_render_mask_.push_back(true);
+  return render_panel_list_.size()-1;
+}
+
+void VisualizationManager::updateRenderMask(int id, bool mask)
+{
+  if(id >= 0 && id < render_panel_render_mask_.size()) {
+    render_panel_render_mask_[id] = mask;
   }
 }
 
@@ -579,6 +614,12 @@ double VisualizationManager::getWallClockElapsed()
 double VisualizationManager::getROSTimeElapsed()
 {
   return static_cast<double>(ros_time_elapsed_) / 1e9;
+}
+
+void VisualizationManager::updateVisibilityMask()
+{
+  visibility_property_->update();
+  queueRender();
 }
 
 void VisualizationManager::updateBackgroundColor()
