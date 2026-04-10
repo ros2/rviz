7efc9085804cb6f35607b9e1758396b3ae9aab27
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2026-04-10 09:52:00 +0200

    Removed warnings (#1693)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- rviz_common/src/rviz_common/visualization_frame.cpp
+++ rviz_common/src/rviz_common/visualization_frame.cpp
@@ -457,17 +457,14 @@ void VisualizationFrame::initMenus()
   file_menu_ = menuBar()->addMenu("&File");
 
   QAction * file_menu_open_action = file_menu_->addAction(
-    "&Open Config", this, SLOT(
-      onOpen()), QKeySequence("Ctrl+O"));
+    "&Open Config", QKeySequence("Ctrl+O"), this, SLOT(onOpen()));
   this->addAction(file_menu_open_action);
   QAction * file_menu_save_action = file_menu_->addAction(
-    "&Save Config", this, SLOT(
-      onSave()), QKeySequence("Ctrl+S"));
+    "&Save Config", QKeySequence("Ctrl+S"), this, SLOT(onSave()));
   this->addAction(file_menu_save_action);
   QAction * file_menu_save_as_action =
     file_menu_->addAction(
-    "Save Config &As", this, SLOT(onSaveAs()),
-    QKeySequence("Ctrl+Shift+S"));
+    "Save Config &As", QKeySequence("Ctrl+Shift+S"), this, SLOT(onSaveAs()));
   this->addAction(file_menu_save_as_action);
 
   recent_configs_menu_ = file_menu_->addMenu("&Recent Configs");
@@ -479,8 +476,7 @@ void VisualizationFrame::initMenus()
   file_menu_->addSeparator();
 
   QAction * file_menu_quit_action = file_menu_->addAction(
-    "&Quit", this, SLOT(
-      close()), QKeySequence("Ctrl+Q"));
+    "&Quit", QKeySequence("Ctrl+Q"), this, SLOT(close()));
   this->addAction(file_menu_quit_action);
 
   view_menu_ = menuBar()->addMenu("&Panels");
@@ -489,8 +485,7 @@ void VisualizationFrame::initMenus()
   delete_view_menu_->setEnabled(false);
 
   QAction * fullscreen_action = view_menu_->addAction(
-    "&Fullscreen", this, SLOT(
-      setFullScreen(bool)), Qt::Key_F11);
+    "&Fullscreen", QKeySequence(Qt::Key_F11), this, SLOT(setFullScreen(bool)));
   fullscreen_action->setCheckable(true);
   this->addAction(fullscreen_action);  // Also add to window, or the shortcut doest work
                                        // when the menu is hidden.
