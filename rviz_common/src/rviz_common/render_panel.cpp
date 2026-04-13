fd2b8140d99601b38c2bfef96e4c1e490b3ff510
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-04-13 22:47:56 +0200

    more fixes
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- rviz_common/src/rviz_common/render_panel.cpp
+++ rviz_common/src/rviz_common/render_panel.cpp
@@ -160,8 +160,13 @@ void RenderPanel::onRenderWindowMouseEvents(QMouseEvent * event)
   int last_x = mouse_x_;
   int last_y = mouse_y_;
 
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
   mouse_x_ = event->position().x();
   mouse_y_ = event->position().y();
+#else
+  mouse_x_ = event->pos().x();
+  mouse_y_ = event->pos().y();
+#endif
 
   if (context_) {
     setFocus(Qt::MouseFocusReason);
@@ -215,8 +220,8 @@ void RenderPanel::wheelEvent(QWheelEvent * event)
   mouse_x_ = rounded_position.x();
   mouse_y_ = rounded_position.y();
 #else
-  mouse_x_ = event->position().x();
-  mouse_y_ = event->position().y();
+  mouse_x_ = event->pos().x();
+  mouse_y_ = event->pos().y();
 #endif
 
   if (context_) {
--- rviz_common/src/rviz_common/viewport_mouse_event.cpp
+++ rviz_common/src/rviz_common/viewport_mouse_event.cpp
@@ -43,8 +43,13 @@ ViewportMouseEvent::ViewportMouseEvent(RenderPanel * p, QMouseEvent * e, int lx,
 : panel(p),
   type(e->type()),
   device_pixel_ratio(static_cast<int>(panel->getRenderWindow()->devicePixelRatio())),
+#if (QT_VERSION >= QT_VERSION_CHECK(5, 14, 0))
   x(e->position().x() * device_pixel_ratio),
   y(e->position().y() * device_pixel_ratio),
+#else
+  x(e->pos().x() * device_pixel_ratio),
+  y(e->pos().y() * device_pixel_ratio),
+#endif
   wheel_delta(0),
   acting_button(e->button()),
   buttons_down(e->buttons()),
@@ -62,8 +67,8 @@ ViewportMouseEvent::ViewportMouseEvent(RenderPanel * p, QWheelEvent * e, int lx,
   x(e->position().x() * device_pixel_ratio),
   y(e->position().y() * device_pixel_ratio),
 #else
-  x(e->position().x() * device_pixel_ratio),
-  y(e->position().y() * device_pixel_ratio),
+  x(e->pos().x() * device_pixel_ratio),
+  y(e->pos().y() * device_pixel_ratio),
 #endif
   wheel_delta(e->angleDelta().y()),
   acting_button(Qt::NoButton),
