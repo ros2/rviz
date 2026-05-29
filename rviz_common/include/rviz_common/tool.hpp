d96b196d88c9c6554acd2313274bff8214663d5f
Alejandro Hernández Cordero <alejandro@openrobotics.org>
2026-05-29 09:48:00 +0200

    Removed deprecated API (#1731)
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- rviz_common/include/rviz_common/tool.hpp
+++ rviz_common/include/rviz_common/tool.hpp
@@ -108,14 +108,6 @@ public:
    */
   virtual void update(std::chrono::nanoseconds wall_dt, std::chrono::nanoseconds ros_dt);
 
-  /// Called periodically, typically at 30Hz.
-  /**
-   * \param wall_dt Wall-clock time, in nanoseconds, since the last time the update list was run through.
-   * \param ros_dt ROS time, in nanoseconds, since the last time the update list was run through.
-   */
-  [[deprecated("Use update(std::chrono::nanoseconds, std::chrono::nanoseconds)")]]
-  virtual void update(float wall_dt, float ros_dt);
-
   enum
   {
     Render = 1,
