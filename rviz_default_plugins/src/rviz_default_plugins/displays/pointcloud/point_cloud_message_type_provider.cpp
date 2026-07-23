d64a0f158e678b0b44e1b9d7035b121c2d01fb33
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-07-23 12:14:53 +0200

    Added PointCloud2 transport type from Add topic display
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

---
+++ rviz_default_plugins/src/rviz_default_plugins/displays/pointcloud/point_cloud_message_type_provider.cpp
@@ -0,0 +1,56 @@
+// Copyright (c) 2026, Open Source Robotics Foundation, Inc.
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+//    * Redistributions of source code must retain the above copyright
+//      notice, this list of conditions and the following disclaimer.
+//
+//    * Redistributions in binary form must reproduce the above copyright
+//      notice, this list of conditions and the following disclaimer in the
+//      documentation and/or other materials provided with the distribution.
+//
+//    * Neither the name of the copyright holder nor the names of its
+//      contributors may be used to endorse or promote products derived from
+//      this software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+
+#include "rviz_default_plugins/displays/pointcloud/point_cloud_message_type_provider.hpp"
+
+#include "rviz_default_plugins/displays/pointcloud/point_cloud_transport_discovery.hpp"
+
+namespace rviz_default_plugins
+{
+namespace displays
+{
+
+QMap<QString, QSet<QString>> PointCloudMessageTypeProvider::getMessageTypes()
+{
+  QSet<QString> message_types;
+  for (const auto & plugin : discoverPointCloudTransportSubscriberPlugins()) {
+    if (!plugin.message_type.empty()) {
+      message_types.insert(QString::fromStdString(plugin.message_type));
+    }
+  }
+  return {{"rviz_default_plugins/PointCloud2", message_types}};
+}
+
+}  // namespace displays
+}  // namespace rviz_default_plugins
+
+#include <pluginlib/class_list_macros.hpp>  // NOLINT
+PLUGINLIB_EXPORT_CLASS(
+  rviz_default_plugins::displays::PointCloudMessageTypeProvider,
+  rviz_common::MessageTypeProvider)
